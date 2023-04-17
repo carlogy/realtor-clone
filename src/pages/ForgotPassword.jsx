c90cebe3d0e922abd7788a1bf4fff9834c7d3530
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-17 18:17:33 -0400

    complete sign in functionality.

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -25,8 +25,8 @@ export default function ForgotPassword() {
                 <form>
                     
                     <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
-                    type='email 
-                    id=email' 
+                    type='email' 
+                    id='email' 
                     value={email} 
                     onChange={onChange}
                     placeholder='Email Address'/> 
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import{AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, auth, getAuth } from 'firebase/auth';
+import {toast} from 'react-toastify';
 
 
 export default function SignIn() {
@@ -12,12 +14,31 @@ export default function SignIn() {
     });
 
     const {email, password} = formData;
+    const navigate = useNavigate();
     function onChange(e){
         setFormData((prevState)=>({
             ...prevState,
             [e.target.id]:e.target.value,
 
         }))
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            if(userCredential.user) {
+                navigate("/")
+            }
+
+            
+        } catch (error) {
+            toast.error("Bad User Credentials")
+            
+        }
+    
     }
   return (
     <section>
@@ -27,11 +48,11 @@ export default function SignIn() {
                 <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80" alt="keys"  className='w-full rounded-2xl '/>
             </div>
             <div className="w-full md:w-[67%] lg:w-[40%] lg:-ml-20">
-                <form>
+                <form onSubmit={onSubmit}>
                     
                     <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
-                    type='email 
-                    id=email' 
+                    type='email' 
+                    id='email' 
                     value={email} 
                     onChange={onChange}
                     placeholder='Email Address'/> 
