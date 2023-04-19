9008312f890a5d8ebe0956fe23906a6dc760e955
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-19 13:40:48 -0400

    added copy and sharing functionality to listing page.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,11 +6,13 @@ import Spinner from '../components/Spinner';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from "swiper"
 import "swiper/css/bundle"
+import {FaShare} from 'react-icons/fa'
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
     SwiperCore.use([Autoplay, Navigation, Pagination]);
     useEffect(() => {
         async function fetchListing(){
@@ -50,7 +52,23 @@ export default function Listing() {
 
         ))}
         </Swiper>
+        <div className='fixed top-[8%] right-[1%] z-10
+         bg-white cursor-pointer border-2 border-gray-400 
+         rounded-full w-12 h-12 flex justify-center items-center'
+        onClick={() => {
+            navigator.clipboard.writeText(window.location.href)
+            setShareLinkCopied(true)
+            setTimeout(() => {
+               setShareLinkCopied(false)
+            }, 2000);
+        } }
+        >
+            <FaShare className='text-lg text-slate-500' />
+        </div>
+        {shareLinkCopied && (
+            <p className='fixed top-[13.2%] right-[4%] font-semi border-2 border-gray-400 rounded-md bg-white z-10 p-2' >Link copied</p>
         
+        )}       
         
         </main>
   )
