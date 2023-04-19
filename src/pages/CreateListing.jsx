dc93d3acbc241d6dcb912d5eb351cad5f03473f2
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-18 20:33:24 -0400

    Added the My Listings sections, and created listingItem component.

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -141,6 +141,7 @@ export default function CreateListing() {
         imgUrls,
         geolocation,
         timestamp: serverTimestamp(),
+        userRef: auth.currentUser.uid,
      };
      
      delete formDataCopy.images;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import { getAuth, signOut, updateProfile } from 'firebase/auth';
-import { doc, updateDoc } from 'firebase/firestore';
-import React, { useState } from 'react'
+import { collection, doc, getDocs, query, updateDoc, where, orderBy } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
 import {FcHome} from "react-icons/fc"
+import ListingItem from '../components/ListingItem';
+
 
 export default function Profile() {
   const auth = getAuth()
@@ -12,10 +14,11 @@ export default function Profile() {
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
-    const {name, email} = formData;
-    
+    const {name, email} = formData;    
     const navigate = useNavigate();
     const [changeDetail, setChangeDetail] = useState(false)
+    const [listings, setListings] = useState(null);
+    const [loading, setLoading] = useState(true);
     function onChange(e) {
       setFormData((prevState) => ({
         ...prevState, 
@@ -45,6 +48,24 @@ export default function Profile() {
      auth.signOut();
      navigate("/")
     }
+    useEffect(() => {
+      async function fetchUserListings() {
+        const listingRef = collection(db, "listings");
+        const q = query(listingRef, where("userRef", "==", auth.currentUser.uid)
+        , orderBy("timestamp", "desc"));
+        const querySnap = await getDocs(q);
+        let listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listings)
+        setLoading(false)
+      }
+      fetchUserListings();
+    }, [auth.currentUser.uid])
   return (
     <>
     <section className='max-w-6xl mx-auto flex justify bg-center items-center flex-col'>
@@ -79,6 +100,21 @@ export default function Profile() {
         </button>
       </div>
     </section>
+    <div className='max-w-6xl px-3 mt-6 mx-auto'>
+      {!loading && listings.length > 0 && (
+        <>
+        <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
+        <ul>
+          {listings.map((listing) => (
+           <ListingItem           
+           key={listing.id} 
+           id={listing.id} 
+           listing={listing.data} />
+         ))}
+        </ul>
+        </>
+      )}
+    </div>
     </>
   )
 }
